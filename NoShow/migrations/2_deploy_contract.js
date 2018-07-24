d8381cf11f2c56aa9dc2279bb8897c033e38e03c
dean-kim <keunu07@gmail.com>
2018-07-25 08:08:22 +0900

    add RestaurantsOwnerRegister in migration

--- NoShow/migrations/2_deploy_contract.js
+++ NoShow/migrations/2_deploy_contract.js
@@ -2,8 +2,10 @@
  * Created by KEUNU on 2018. 6. 8..
  */
 const NoShow = artifacts.require("NoShow");
+const RestaurantsOwnerRegister = artifacts.require("RestaurantsOwnerRegister");
 
 module.exports = (deployer) => {
-    deployer.deploy(NoShow)
+    deployer.deploy(NoShow);
+    deployer.deploy(RestaurantsOwnerRegister);
 };
 
